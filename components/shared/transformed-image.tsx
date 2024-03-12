2b325fc74e96352127f5222e86d215f3549384ac
AlexandreDresch <connectWithAlexandre@proton.me>
2024-03-11 22:12:14 -0300

    feat: add downloadHandler function

--- components/shared/transformed-image.tsx
+++ components/shared/transformed-image.tsx
@@ -1,5 +1,7 @@
-import { dataUrl, debounce, getImageSize } from "@/lib/utils";
-import { CldImage } from "next-cloudinary";
+"use client";
+
+import { dataUrl, debounce, download, getImageSize } from "@/lib/utils";
+import { CldImage, getCldImageUrl } from "next-cloudinary";
 import { PlaceholderValue } from "next/dist/shared/lib/get-img-props";
 import Image from "next/image";
 import React from "react";
@@ -13,7 +15,17 @@ export default function TransformedImage({
   setIsTransforming,
   hasDownload = false,
 }: TransformedImageProps) {
-  function downloadHandler() {}
+  function downloadHandler(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
+    
+    download(getCldImageUrl({
+      width: image?.width,
+      height: image?.height,
+      src: image?.publicId,
+      ...transformationConfig
+    }), title)
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex-between">
@@ -48,19 +60,20 @@ export default function TransformedImage({
             onError={() => {
               debounce(() => {
                 setIsTransforming && setIsTransforming(false);
-              }, 8000);
+              }, 8000)();
             }}
             {...transformationConfig}
           />
 
           {isTransforming && (
             <div className="transforming-loader">
-                <Image 
-                    src="/assets/icons/spinner.svg"
-                    width={50}
-                    height={50}
-                    alt="Transforming"
-                />
+              <Image
+                src="/assets/icons/spinner.svg"
+                width={50}
+                height={50}
+                alt="Spinner"
+              />
+              <p className="text-white/80">Please wait...</p>
             </div>
           )}
         </div>
